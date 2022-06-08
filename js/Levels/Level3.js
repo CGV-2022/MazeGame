5e829358e52e1dca15ec16b840274583713e8f64
Aashna <2202103@students.wits.ac.za>
2022-06-08 16:05:41 +0200

    Updated Level 3 Lighting

--- js/Levels/Level3.js
+++ js/Levels/Level3.js
@@ -79,13 +79,13 @@ function Level3Init() {
     //camera.position.set(0, 150, 0);                            //level editing
 
     //RENDERER
-    //var options = { antialias: true };
-    var options = { antialias: false };
+    var options = { antialias: true };
+    //var options = { antialias: false };
     const renderer = new THREE.WebGLRenderer(options);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio/* *0.8 */);
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;            //leave commented for better perf
+    //renderer.shadowMap.type = THREE.PCFSoftShadowMap;            //leave commented for better perf
 
     //CONTROLS
     const orbitControls = new OrbitControls(camera, renderer.domElement);
@@ -95,7 +95,6 @@ function Level3Init() {
     orbitControls.minDistance = 6;                               //at origin    
     orbitControls.maxDistance = 6;                               //for level editing                
     orbitControls.update();
-
 
     //MINIMAP
     const camera2 = new THREE.OrthographicCamera(-10, 10, 10, -10, 0, 100);
@@ -239,24 +238,24 @@ function Level3Init() {
     var torchModel;
 
     const managerTorch = new THREE.LoadingManager();
-    managerTorch.onLoad = function() { 
+    managerTorch.onLoad = function () {
         //when torch model has been loaded. Can clone a bunch of torches in here
         //ading torches to walls
         torch(new THREE.Vector3(-12, 5, 31));
         torch(new THREE.Vector3(-9, 5, 31));
         torch(new THREE.Vector3(-9, 5, -8));
-        torch(new THREE.Vector3(15, 5, -25));
+        torch(new THREE.Vector3(15, 5, -28));
         torch(new THREE.Vector3(15, 5, 38));
-        
+
     }
 
     const loaderTorch = new FBXLoader(managerTorch);
     loaderTorch.setPath('./Resources/models/Torch/');
     loaderTorch.load('Torch.fbx', (fbx) => {
-    const model = fbx;
-    fbx.scale.setScalar(0.02);
+        const model = fbx;
+        fbx.scale.setScalar(0.02);
 
-    torchModel = model;
+        torchModel = model;
     });
 
 
@@ -442,17 +441,17 @@ function Level3Init() {
     const clock = new THREE.Clock();
     var paused = false;
     document.addEventListener('keypress', (event) => {
-        if (event.key.toLowerCase()=='p') {
+        if (event.key.toLowerCase() == 'p') {
             paused = !paused;
 
-            if(paused) {
+            if (paused) {
                 stopLevelTimer();
                 clock.stop();
                 ps.style.display = 'flex';
-                
-               
+
+
                 //ps.textContent="PAUSED";
-                
+
             }
             else {
                 startLevelTimer();
@@ -461,7 +460,7 @@ function Level3Init() {
                 //ps.textContent="";
             }
         }
-        
+
     }, false);
 
 
@@ -530,10 +529,10 @@ function Level3Init() {
     //LIGHTS
     function mainLighting() {
         //ambient light
-        scene.add(new THREE.AmbientLight(0xffffff, 0.1));
+        scene.add(new THREE.AmbientLight(0xffffff, 0.2));
 
         //directional light
-        const dirLight = new THREE.DirectionalLight(0xDD8B41, 0.3);
+        const dirLight = new THREE.DirectionalLight(0xDD8B41, 0.2);
         dirLight.position.set(-120, 80, 0);
         dirLight.castShadow = true;
         dirLight.shadow.camera.top = 50;
@@ -627,7 +626,7 @@ function Level3Init() {
     spotlight.intesity = 50;
     spotlight.angle = Math.PI / 8;
     spotlight.penumbra = 0.1;
-    spotlight.decay = 20;
+    //spotlight.decay = 20;
     spotlight.distance = 600;
 
     camera.add(spotlight);
@@ -649,18 +648,17 @@ function Level3Init() {
     scene.add(camera);
 
     //TORCHES
-   //The light emitted from the torch
-   function torch(lightPos) 
-   {
-    //mesh
-    var model = torchModel.clone();
-    model.position.set(lightPos.x, lightPos.y, lightPos.z);
-    scene.add(model);
+    //The light emitted from the torch
+    function torch(lightPos) {
+        //mesh
+        var model = torchModel.clone();
+        model.position.set(lightPos.x, lightPos.y, lightPos.z);
+        scene.add(model);
 
-     //light colour and intensity
-    const light = new THREE.PointLight('orange', 6, 10, 2);
-    light.castShadow = true;
-    light.position.copy(lightPos);
-    scene.add(light); 
-    } 
+        //light colour and intensity
+        const light = new THREE.PointLight('orange', 6, 10, 2);
+        light.castShadow = true;
+        light.position.copy(lightPos);
+        scene.add(light);
+    }
 }
